25e8953c92fb4a653685d1b1149fc12ceaed3b8f
Dominic Plein <dominic-plein@gmx.de>
2018-05-09 20:38:49 +0200

    Add documentation

--- src/com/yamaha/model/editor/RMGroup.java
+++ src/com/yamaha/model/editor/RMGroup.java
@@ -1,5 +1,10 @@
 package com.yamaha.model.editor;
 
+/**
+ * A RMGroup represents a group of information to be saved in the Registration Memory.
+ * <p>Note: Do not rename the enum fields because they are used via the MenuBarController to load the linked .fxml
+ * files!
+ */
 public enum RMGroup { // extend this enum
 
     TITLE,
